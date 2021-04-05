643cd142e9ff67845e40245737ce45f3c16a918b
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-04-05 15:33:11 +0300

    ➕ Add Firebase for Android and Web

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # arrowad_grade_eleven
+
+You need to add `android/app/google-services.json`
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:arrowad_grade_eleven/src/app/app.dart';
 import 'package:arrowad_grade_eleven/src/app/locator/locator.dart';
+import 'package:arrowad_grade_eleven/src/app/services/firebase_service.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
-  await configureDependencies();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  configureDependencies();
+
+  await locator<FirebaseService>().init();
+
   runApp(App());
 }
